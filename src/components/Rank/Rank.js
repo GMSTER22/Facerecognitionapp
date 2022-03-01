a58378cc8dc0370390e2ee68e7608c2e856ede5d
Gael Souleymane Nongnogo <gmster22@gmail.com>
2022-03-01 18:24:49 -0500

    added particlesJs to the project

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Rank.css"
+
+function Rank() {
+    return (
+        <div className="rank">
+            <div className="white f2">
+                {"Andrei, your current rank is..."}
+            </div>
+            <div className="white f1">
+                {"#5"}
+            </div>
+        </div>
+    )
+}
+
+export default Rank;
